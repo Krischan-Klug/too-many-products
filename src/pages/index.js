c77879e0e36435f6a3e8cb68e34be7fd7c0486b6
Krischan-Klug <krischanklug@icloud.com>
2024-09-09 20:55:52 +0200

    add logout button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <h1>HOME</h1>
       <p> Logged in User: {session.user.username}</p>
+      <button onClick={() => signOut()}>Logout</button>
     </>
   );
 }
